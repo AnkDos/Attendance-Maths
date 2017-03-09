d5201d694341fc8da37df064436128c1c98dcd94
AnkDos <ankurpan96@gmail.com>
2017-03-09 23:05:27 +0530

    Made it object oriented

--- Attendance-Maths.java
+++ Attendance-Maths.java
@@ -5,7 +5,25 @@ import java.util.*;
 
 class Calc{
 
-static  double attnCalc(double present,double total){
+public static void main (String args[]){
+Scanner n=new Scanner(System.in);
+System.out.println("Enter Present Hours");
+double present_hours=Double.parseDouble(""+n.nextDouble());
+System.out.println("Enter Total Hours");
+double total_hours=Double.parseDouble(""+n.nextDouble());
+Att obj=new Att();
+double result=obj.attnCalc(present_hours,total_hours);
+System.out.println("You Need To be present in next  "+result+" Hours To hit 75%");
+}
+
+}
+
+class Att{
+  private double pre;
+  private double ttl;
+public  double attnCalc(double present,double total){
+    pre =present;
+    ttl=total;
   double present_attendance=present/total*100;
   int count=0,num=1,denom=1;
   while (true)
@@ -20,17 +38,4 @@ count++;
   }
   return count;
   }
-
-
-public static void main (String args[]){
-Scanner n=new Scanner(System.in);
-System.out.println("Enter Present Hours");
-double present_hours=Double.parseDouble(""+n.nextDouble());
-System.out.println("Enter Total Hours");
-double total_hours=Double.parseDouble(""+n.nextDouble());
-
-double result=attnCalc(present_hours,total_hours);
-System.out.println("You Need "+result+" Hours To hit 75%");
-}
-
 }
