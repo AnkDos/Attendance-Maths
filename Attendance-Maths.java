9a1a535d798cae8089097433c184de8b793c44a0
AnkDos <ankurpan96@gmail.com>
2017-03-10 00:43:12 +0530

    Now It can Predict what more number of classes we can bunk

--- Attendance-Maths.java
+++ Attendance-Maths.java
@@ -12,30 +12,70 @@ double present_hours=Double.parseDouble(""+n.nextDouble());
 System.out.println("Enter Total Hours");
 double total_hours=Double.parseDouble(""+n.nextDouble());
 Att obj=new Att();
-double result=obj.attnCalc(present_hours,total_hours);
-System.out.println("You Need To be present in next  "+result+" Hours To hit 75%");
+obj.attnCalc(present_hours,total_hours);
+obj.predict();
+obj.display();
+
 }
 
 }
 
 class Att{
+
   private double pre;
   private double ttl;
-public  double attnCalc(double present,double total){
+  double main_attendance;
+//Variables
+int count=0,num=1,denom=1;
+int countp=0,deno=1;
+//****************************************************************************
+  public  double attnCalc(double present,double total){
     pre =present;
     ttl=total;
-  double present_attendance=present/total*100;
-  int count=0,num=1,denom=1;
+  main_attendance=pre/ttl*100;
+
+
   while (true)
    {
 
-       present_attendance=((present+num)/(total+denom))*100;
+      double current_attendance=((present+num)/(total+denom))*100;
        num++;
        denom++;
 
-   if(present_attendance>75){break;}
+   if(current_attendance>75){break;}
 count++;
   }
   return count;
   }
+//****************************************************************************
+ public double predict(){
+
+if(main_attendance>75)
+{
+while (deno<=1000)
+ {
+
+    double predicted_attandance=((pre)/(ttl+deno))*100;
+if(predicted_attandance>=75){countp++;}
+     deno++;
+}
+
+}
+return countp;
+}
+//****************************************************************************
+void display(){
+  if(main_attendance>=75){
+    System.out.println("You are Safe :) ");
+    System.out.println("In case Of Emergency,You can bunk "+countp+" Consecutive classes(if taken) and will stay above or equal 75%");
+  }
+  if(main_attendance<75){
+    System.out.println("You Need To be present in next  "+count+" Hours To hit 75%");
+  }
+}
+
+
+
+//****************************************************************************
+
 }
